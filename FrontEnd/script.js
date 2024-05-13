df69e854f3daf9c41bc2c271df09c046b1167cb0
Marwa Derbel <marwaderbel21@gmail.com>
2024-05-13 15:15:08 +0200

    add

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,22 +1,42 @@
-async function getworks (){
-    const response = await fetch ("http://localhost:5678/api/works",{
-        method:'get',  
-        headers: {  
-        "Accept": "application/json"    
-        }     
-       }    
-   )
+// connexion API
+const token=sessionStorage.getItem("accessToken"); 
+//function to get all the data from the database
+async function dataBaseInfo (type){
+    const response = await fetch ("http://localhost:5678/api/" + type);   
+   
     if (response.ok == true){
+        console.log("connexion database : successful");
         return (await response).json();
+    } else {
+        console.log("Error Connexion API:" + reponse.Error);
     }
-    throw new Error("unapel to acess")
 }
-let worksliste =null;
+console.log(dataBaseInfo("works"));
+console.log(dataBaseInfo("categories"));
 
-getworks().then(data => {
-    worksliste = data;
-    console.log(worksliste);
+// DOM Elements
+const gallery=document.getElementById("gallery");
+const filters =document.getElementById("filters");
+const modalContainer=document.querySelector(".modalContainer");
+const worksContainer = document.querySelector('.worksContainer');
+
+//Gallery
+function showworksInModal(){
+    worksContainer.innerHTML="";
+    //show all works in array
+    dataworks.forEach((work) => {
+        //create tags
+        const workCard=document.createElement("figure");
+        const workImage=document.createElement("img");
+        //get the data from the database
+        workCard.dataset.id=work.id;
+        workImage.src=work.imageUrl;
+        workImage.alt=work.titel;
+        //ref to DOM
+        gallery.appenchild(workCard);
+        workCard.append(workImage, workTitle);   
+    });
+    
 }
-)
 
 
