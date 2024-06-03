415793655b6de1117fea4677697993a3153330c9
Marwa Derbel <marwaderbel21@gmail.com>
2024-06-03 09:43:26 +0200

    add

---
+++ FrontEnd/login.js
@@ -0,0 +1,37 @@
+//export function Form(){
+    const form = document.getElementById("connexion");
+    console.log (form);
+    
+    // ---------- EventListener ----------
+    form.addEventListener("click", async (e) => {
+      // Get the user input
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+    
+      // Prevent the form from auto-submitting
+      e.preventDefault();
+    
+      // Request POST to login with the user input
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }), // Add user input into request body
+      })
+      
+      // Check if the previous request is successful
+      if (response.ok === true) {
+        const data = await response.json(); // Get data from response
+        sessionStorage.setItem("accessToken", data.token); // Save the token in sessionStorage
+        window.location.href = "./index.html"; // Redirect to index.html
+        console.log("Connexion mode admin : Successful");
+      } else {
+        const connexion = document.querySelector("div"); // Select HTML element where to display the error message
+        const error = document.createElement("p"); // Create HTML element to display to display the error message
+        error.innerText = "Erreur dans l’identifiant ou le mot de passe";
+        error.style.color = "red";
+        error.style.textAlign = "center";
+        error.style.marginBottom = "25px";
+        connexion.insertBefore(error, connexion.lastElementChild); // Position the error message
+      };
+    });
+
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,5 @@
+//import{Form} from "./login.js";
+const token = sessionStorage.getItem("accessToken");
 //Data 
 const baseInfo = "http://localhost:5678/api/";
 let worksData;
@@ -6,8 +8,7 @@ let categories;
 //Elements
 let gallery;
 let filter;
-let modal;
-let modalStep = null;
+let modal=null
 let pictureInput;
 
 // FETCH works data from API and display it
@@ -24,11 +25,9 @@ window.onload = () => {
       filter = document.querySelector(".filter");
       categoryFilter(categories, filter);
       //administrator mode
-      adminUserMode(filter);
+
     });
-};
-
-
+}
 //*******GALLERY*******
 
 function getworks(data) {
@@ -50,64 +49,125 @@ function getworks(data) {
     workCard.appendChild(workImage);
     workImage.appendChild(workTitle);
   });
-  }
+}
 //get list of categories in array as unique objects
 function listOfUniqueCategories() {
-    let listOfCategories = new Set();
-    //get set of string categories
-    worksData.forEach((work) => {
-      listOfCategories.add(JSON.stringify(work.category));
+  let listOfCategories = new Set();
+  //get set of string categories
+  worksData.forEach((work) => {
+    listOfCategories.add(JSON.stringify(work.category));
+  });
+  //push stringified categories in array
+  const arrayOfStrings = [...listOfCategories];
+  //parse array to get objects back
+  categories = arrayOfStrings.map((s) => JSON.parse(s));
+}
+//filter buttons
+function categoryFilter(categories, filter) {
+  const button = document.createElement("button");
+  button.innerText = "Tous";
+  button.className = "filterButton";
+  button.dataset.category = "Tous";
+  filter.appendChild(button);
+  filtreButtons(categories, filter);
+  functionFilter();
+}
+//create filter buttons
+function filtreButtons(categories, filter) {
+  categories.forEach((categorie) => {
+    createButtonFilter(categorie, filter);
+  });
+}
+function createButtonFilter(categorie, filter) {
+  const button = document.createElement("button");
+  button.innerText = categorie.name;
+  button.className = "filterButton";
+  button.dataset.category = categorie.name;
+  filter.appendChild(button);
+}
+// Gallery filter
+function functionFilter() {
+  const filterButtons = document.querySelectorAll(".filterButton");
+  //identify wich filter button has been clicked
+  filterButtons.forEach((i) => {
+    i.addEventListener("click", function () {
+      toggleProjects(i.dataset.category);
     });
-    //push stringified categories in array
-    const arrayOfStrings = [...listOfCategories];
-    //parse array to get objects back
-    categories = arrayOfStrings.map((s) => JSON.parse(s));
-  }
-  //filter buttons
-  function categoryFilter(categories, filter){
-    const button=document.createElement("button");
-    button.innerText="Tous";
-    button.className="filterButton";
-    button.dataset.category="Tous";
-    filter.appendChild(button);
-    filtreButtons(categories,filter);
-    functionFilter();
-  }
-  //create filter buttons
-  function filtreButtons(categories,filter){
-        categories.forEach((categorie) => {
-          createButtonFilter(categorie, filter);
-        });
-  }
-  function createButtonFilter(categorie, filter) {
-    const button = document.createElement("button");
-    button.innerText = categorie.name;
-    button.className = "filterButton";
-    button.dataset.category = categorie.name;
-    filter.appendChild(button);
-  }
-  // Gallery filter
-  function functionFilter() {
-    const filterButtons = document.querySelectorAll(".filterButton");
-    //identify wich filter button has been clicked
-    filterButtons.forEach((i) => {
-      i.addEventListener("click", function () {
-        toggleProjects(i.dataset.category);
-      });
-    });
-  } 
+  });
+}
 //if button "tous" active, display all projects, else display only those with same dataset category
 function toggleProjects(datasetCategory) {
-    const figures = document.querySelectorAll(".workCard");
-    if ("Tous" === datasetCategory) {
-      figures.forEach((figure) => {
-        figure.style.display = "block";
-      });
-    } else {
-      figures.forEach((figure) => {
-        figure.dataset.category === datasetCategory
-          ? (figure.style.display = "block")
-          : (figure.style.display = "none");
-      });
-    }
+  const figures = document.querySelectorAll(".workCard");
+  if ("Tous" === datasetCategory) {
+    figures.forEach((figure) => {
+      figure.style.display = "block";
+    });
+  } else {
+    figures.forEach((figure) => {
+      figure.dataset.category === datasetCategory
+        ? (figure.style.display = "block")
+        : (figure.style.display = "none");
+    });
   }
+};
+
+
+//MODAL
+
+const openModal = function (e) {
+  e.preventDefault()
+  modal = document.querySelector(e.target.getAttribute('href'))
+  modal.style.display = null
+  modal.removeAttribute('aria-hidden')
+  modal.setAttribute('aria-modal', 'true')
+  modal.addEventListener('click', closModal)
+  modal.querySelector('js-close-modal').addEventListener('click', closModal)
+  modal.querySelector('js-modal-stop').addEventListener('click', stopPropagation)
+  functionModalVue2()
+}
+
+const closModal = function (e) {
+  if (modal === null) return
+  e.preventDefault()
+  modal.style.display = "none"
+  modal.removeAttribute('aria-hidden', 'true')
+  modal.removeEventListener('click', closModal)
+  modal.querySelector('js-close-modal').removeEventListener('click', closModal)
+  modal.querySelector('js-modal-stop').removeEventListener('clik', stopPropagation)
+  modal = null
+}
+const stopPropagation = function (e) {
+  e.stopPropagation()
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+  a.addEventListener('click', openModal)
+})
+
+window.addEventListener('keydown', function (e){
+  if (e.key === "Escape" || e.key === "Esc") {
+    closModal(e)
+  }
+  })
+
+  function functionModalVue2 (){
+    document.getElementById("icone-return").addEventListener("click", btnRetourModalVue2)
+    document.querySelector(".btn-add-photo-modal").addEventListener("click", closeModalVue2)
+    document.querySelector('.js-close-modal-vue2').addEventListener('click', closModal)
+}
+
+function btnRetourModalVue2(){
+  workPicture = undefined;
+  document.querySelector(".js-container-modal").style.display = null;
+  document.querySelector(".container-add-photo-modal").style.display = "none"
+  document.querySelector(".container-photo").style.display = null
+  document.querySelector(".photo").textContent = null
+}
+function closeModalVue2() {
+  document.querySelector(".js-container-modal").style.display = "none";
+  document.querySelector(".container-add-photo-modal").style.display =null
+  sendProject()
+}
+
+
+
