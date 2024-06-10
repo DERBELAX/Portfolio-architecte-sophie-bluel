3297a3a1b9ae500a11a2fae45aef4f79e93fdf92
Marwa Derbel <marwaderbel21@gmail.com>
2024-06-10 09:50:25 +0200

    addmodal

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -6,7 +6,8 @@ let categories;
 //Elements
 let gallery;
 let filter;
-let modal=null
+let modal;
+let modalStep = null;
 let pictureInput;
 
 // FETCH works data from API and display it
@@ -22,8 +23,7 @@ window.onload = () => {
       //Filter functionnality
       filter = document.querySelector(".filter");
       categoryFilter(categories, filter);
-      //administrator mode
-
+      adminUserMode(filter);
     });
 }
 //GALLERY
@@ -31,9 +31,9 @@ window.onload = () => {
 function getworks(data) {
   gallery = document.querySelector(".gallery");
   gallery.innerHTML = "";
-  //show all works in array
+//show all works in array
   data.forEach((i) => {
-    //create tags
+//create tags
     const workCard = document.createElement("figure");
     const workImage = document.createElement("img");
     const workTitle = document.createElement("figcaption");
@@ -44,10 +44,10 @@ function getworks(data) {
     workCard.className = "workCard";
     //references to DOM
     gallery.appendChild(workCard);
-    workCard.appendChild(workImage);
-    workImage.appendChild(workTitle);
+    workCard.append(workImage,workTitle)
   });
 }
+
 //get list of categories in array as unique objects
 function listCategories() {
   let listCategories = new Set();
@@ -86,7 +86,7 @@ function createButtonFilter(categorie, filter) {
 // Gallery filter
 function functionFilter() {
   const filterButtons = document.querySelectorAll(".filterButton");
-  //identify wich filter button has been clicked
+//identify wich filter button has been clicked
   filterButtons.forEach((i) => {
     i.addEventListener("click", function () {
       toggleProjects(i.dataset.category);
@@ -107,63 +107,256 @@ function toggleProjects(datasetCategory) {
         : (figure.style.display = "none");
     });
   }
-};
+}
+//ADMIN MODE
 
+function adminUserMode() {
+  //display admin mode if token is found and has the expected length (optional chaining)
+  if (sessionStorage.getItem("token")?.length == 143) {
+    //Hide filter
+    document.querySelector(".filter").style.display = "none";
+    //change login to logout
+    document.getElementById("logBtn").innerText = "logout";
+    //display top menu bar
+    const body = document.querySelector("body");
+    const topMenu = document.createElement("div");
+    const editMode = document.createElement("p");
+
+    topMenu.className = "topMenu";
+    editMode.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>Mode édition`;
+   
+    body.insertAdjacentElement("afterbegin", topMenu);
+    topMenu.appendChild(editMode);
+    //edit buttons
+    const editBtn = `<p class="editBtn"><i class="fa-regular fa-pen-to-square"></i>Modifier</p>`;
+    document.querySelector("#portfolio h2").insertAdjacentHTML("afterend", editBtn);
+    //event listener modal
+    document.querySelector("#portfolio p").addEventListener("click", openModal);
+  }
+}
 
 //MODAL
 
-const openModal = function (modal) {
-  modal.preventDefault()
-  modal.removeAttribute('aria-hidden')
-  modal.setAttribute('aria-modal', 'true')
-  modal.addEventListener('click', closModal)
-  modal.querySelector('js-close-modal').addEventListener('click', closModal)
-  modal.querySelector('js-modal-stop').addEventListener('click', stopPropagation)
-  functionModalVue2()
-}
-
-const closModal = function (modal) {
-  if (modal === null) return
-  modal.preventDefault()
-  modal.style.display = "none"
-  modal.removeAttribute('aria-hidden', 'true')
-  modal.removeEventListener('click', closModal)
-  modal.querySelector('js-close-modal').removeEventListener('click', closModal)
-  modal.querySelector('js-modal-stop').removeEventListener('clik', stopPropagation)
-  modal = null
-}
-const stopPropagation = function (modal) {
-  modal.stopPropagation()
-}
-
-document.querySelectorAll('.js-modal').forEach(a => {
-  a.addEventListener('click', openModal)
-})
-
-window.addEventListener('keydown', function (modal){
-  if (modal.key === "Escape" || modal.key === "Esc") {
-    closModal(modal)
+//open modal if token is found and has the expected length
+const openModal = function () {
+  if (sessionStorage.getItem("token")?.length == 143) {
+    modal = document.querySelector(".modal");
+    modal.style.display = "flex";
+    document.querySelector("#addPicture").style.display = "none";
+    document.querySelector("#editGallery").style.display = "flex";
+    modalGallery(worksData);
+    modalStep = 0;
+    // close modal listener
+    modal.addEventListener("click", closeModal);
+    // DELETE button listener
+    document.addEventListener("click", deleteBtn);
+    document.addEventListener("click", openNewWorkForm);
   }
+};
+
+//close modal
+const closeModal = function (e) {
+  if (
+    e.target === document.querySelector(".modal") ||
+    e.target === document.getElementsByClassName("fa-xmark")[modalStep]
+  ) {
+    document.querySelector(".modal").style.display = "none";
+    document.removeEventListener("click", closeModal);
+    document.removeEventListener("click", deleteBtn);
+    modalStep = null;
+  }
+}
+
+//*************DELETE***************/
+
+//display modal gallery function
+function modalGallery(data) {
+  const modalContent = document.querySelector(".modalContent");
+  modalContent.innerHTML = "";
+  //show all works in array
+  data.forEach((i) => {
+    //create elements
+    const miniWork = document.createElement("figure");
+    const modalImage = document.createElement("img");
+    const edit = document.createElement("figcaption");
+    const trashCan = document.createElement("i");
+    //trashcan ID is work ID
+    trashCan.id = i.id;
+    trashCan.classList.add("fa-solid", "fa-trash-can");
+    modalImage.src = i.imageUrl;
+    modalImage.alt = i.title;
+    miniWork.className = "miniWork";
+    //references to DOM
+    modalContent.appendChild(miniWork);
+    miniWork.append(modalImage, trashCan);
+  });
+}
+
+//DELETE work event listener handler
+const deleteBtn = function (e) {
+  e.preventDefault();
+  //clicked button
+  if (e.target.matches(".fa-trash-can")) {
+    deleteWork(e.target.id);
+  }
+};
+
+//API call for DELETE route
+function deleteWork(i) {
+  //authentify user and send API response
+  let token = sessionStorage.getItem("token");
+  fetch(baseApiUrl + "works/" + i, {
+    method: "DELETE",
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    //if response is positive, update the works gallery accordingly
+    if (response.ok) {
+      alert("Projet supprimé avec succés")
+      //delete work from worksData array
+      worksData = worksData.filter((work) => work.id != i);
+      //display updated galleries
+      displayGallery(worksData);
+      modalGallery(worksData);
+      //if response is negative report an error
+    } else {
+      alert("Erreur : " + response.status);
+      closeModal;
+    }
+  });
+}
+
+//ADD WORK
+
+//display add work form
+const openNewWorkForm = function (e) {
+  if(e.target === document.querySelector("#addPictureBtn")){
+    modalStep = 1;
+    document.querySelector("#addPicture").style.display = "flex";
+    document.querySelector("#editGallery").style.display = "none";
+    document.querySelector("#labelPhoto").style.display = "flex";
+    document.querySelector("#picturePreview").style.display = "none";
+    document.getElementById("addPictureForm").reset();
+    //<select> categories list 
+    selectCategoryForm();
+    //display preview
+    pictureInput = document.querySelector("#photo");
+    pictureInput.onchange = picturePreview;
+    //events
+    document.addEventListener("click", closeModal);
+    document.querySelector(".modalHeader .fa-arrow-left").addEventListener("click", openModal);
+    document.removeEventListener("click", openNewWorkForm);
+    document.removeEventListener("click", deleteBtn);
+    document.addEventListener("click", newWorkFormSubmit);
+  }
+}
+//preview picture in form
+const picturePreview = function() {
+  const [file] = pictureInput.files;
+  if (file) {
+    document.querySelector("#picturePreviewImg").src = URL.createObjectURL(file);
+    document.querySelector("#picturePreview").style.display = "flex";
+    document.querySelector("#labelPhoto").style.display = "none";
+  }
+};
+
+//category options for form
+const selectCategoryForm = function () {
+  //reset categories
+  document.querySelector("#selectCategory").innerHTML = "";
+  //empty first option
+  option = document.createElement("option");
+  document.querySelector("#selectCategory").appendChild(option);
+  //options from categories array
+  categories.forEach((categorie) => {
+    option = document.createElement("option");
+    option.value = categorie.name;
+    option.innerText = categorie.name;
+    option.id = categorie.id;
+    document.querySelector("#selectCategory").appendChild(option);
+  });
+};
+
+//submit work form event listener
+const newWorkFormSubmit = function (e) {
+  if (e.target === document.querySelector("#valider")) {
+    e.preventDefault();
+    postNewWork();
+  }
+}
+//POST new work
+function postNewWork() {
+  let token = sessionStorage.getItem("token");
+  const select = document.getElementById("selectCategory");
+  //get data from form
+  const title = document.getElementById("title").value;
+  const categoryName = select.options[select.selectedIndex].innerText;
+  const categoryId = select.options[select.selectedIndex].id;
+  const image = document.getElementById("photo").files[0];
+  //check form validity
+  let validity = formValidation(image, title, categoryId);
+  if (validity === true) {
+    //create FormData
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("title", title);
+    formData.append("category", categoryId);
+    // send collected data to API
+    sendNewData(token, formData, title, categoryName);
+  }
+};
+//form validation
+const formValidation = function(image, title, categoryId) {
+  if (image == undefined){
+    alert("Veuillez ajouter une image");
+    return false;
+  }
+  if (title.trim().length == 0){    
+    alert("Veuillez ajouter un titre");
+    return false;
+  }
+  if (categoryId == ""){
+    alert("Veuillez choisir une catégorie");
+    return false;
+  }else{
+  return true;
+  }
+}
+
+//add new work in worksData array for dynamic display using API response
+const addToWorksData = function(data, categoryName) {
+  newWork = {};
+  newWork.title = data.title;
+  newWork.id = data.id;
+  newWork.category = {"id" : data.categoryId, "name" : categoryName};
+  newWork.imageUrl = data.imageUrl;
+  worksData.push(newWork);
+}
+
+//API call for new work
+function sendNewData(token, formData, title, categoryName) {
+  fetch(`${baseApiUrl}works`, {
+    method: "POST",
+    headers: {
+    authorization: `Bearer ${token}`,
+    },
+    body: formData,
   })
-
-  function functionModalVue2 (){
-    document.getElementById("icone-return").addEventListener("click", btnRetourModalVue2)
-    document.querySelector(".btn-add-photo-modal").addEventListener("click", closeModalVue2)
-    document.querySelector('.js-close-modal-vue2').addEventListener('click', closModal)
+    .then((response) => {
+      if (response.ok) {
+        alert("Nouveau fichier envoyé avec succés : " + title);
+        return response.json();
+      } else {
+        console.error("Erreur:", response.status);
+      }
+    })
+    .then ((data) => {
+      addToWorksData(data, categoryName);
+      displayGallery(worksData);
+      document.querySelector(".modal").style.display = "none";
+      document.removeEventListener("click", closeModal);
+      modalStep = null;
+    })
+    .catch((error) => console.error("Erreur:", error));
 }
-
-function btnRetourModalVue2(){
-  workPicture = undefined;
-  document.querySelector(".js-container-modal").style.display = null;
-  document.querySelector(".container-add-photo-modal").style.display = "none"
-  document.querySelector(".container-photo").style.display = null
-  document.querySelector(".photo").textContent = null
-}
-function closeModalVue2() {
-  document.querySelector(".js-container-modal").style.display = "none";
-  document.querySelector(".container-add-photo-modal").style.display =null
-  sendProject()
-}
-
-
-
